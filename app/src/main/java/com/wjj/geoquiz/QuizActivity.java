594b1920af0d288109bf3164340fdffefc58b387
草原神兽 <wangjjwc@163.com>
2017-08-25 18:40:53 +0800

    第三章—>第四节

--- app/src/main/java/com/wjj/geoquiz/QuizActivity.java
+++ app/src/main/java/com/wjj/geoquiz/QuizActivity.java
@@ -1,21 +1,26 @@
 package com.wjj.geoquiz;
 
-import android.graphics.Color;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     // Buttons
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mPrevButton;
-    private Button mNextButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
 
     // TextViews
     private TextView mQuestionTextView;
@@ -34,19 +39,19 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState ==null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         // Buttons
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
 
-        mPrevButton = (Button) findViewById(R.id.prev_button);
-        mPrevButton.setClickable(false);
-        mPrevButton.setTextColor(Color.GRAY);
-
-        mNextButton = (Button) findViewById(R.id.next_button);
-
-        // TextViews
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         updateQuestion();
 
@@ -69,20 +74,17 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
-                mPrevButton.setClickable(true);
-                mPrevButton.setTextColor(Color.BLACK);
             }
         });
 
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, "" + mCurrentIndex, Toast.LENGTH_SHORT).show();
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
-                updateQuestion();
-                if (mCurrentIndex == 0) {
-                    mPrevButton.setTextColor(Color.GRAY);
-                    mPrevButton.setClickable(false);
+                if (mCurrentIndex == 0){
+                    Toast.makeText(QuizActivity.this, "" + "已经是第一个问题了！", Toast.LENGTH_SHORT).show();
+                } else {
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                    updateQuestion();
                 }
             }
         });
@@ -96,9 +98,42 @@ public class QuizActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextId();
         mQuestionTextView.setText(question);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void checkAnswer(boolean userPressedTrue) {
